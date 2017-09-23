420ecabcd5f3b9441d6c3ff2b414097d52af6a73
Jonathan Hall <jonathanm.hall10@gmail.com>
2017-09-23 15:05:16 +1000

    Begin grid arena positioning

--- Game/GridGame.java
+++ Game/GridGame.java
@@ -3,18 +3,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GridGame {
-    static int playerCount = Game.users.size();
 
     public static int[][] theGrid = new int[200][200];
 
     public static void positionUsers(){
         Game.users.add(new User("jef", 2, 1, true));
-        System.out.println(playerCount);
-        for (int i = 0;i < playerCount;i++){
+        System.out.println(Game.users.size());
+        for (int i = 0;i < Game.users.size();i++){
             System.out.println("player" + i);
-    }
         }
     }
+}
 
 
 //            for (int j = 0; j < theGrid[0].length; j++){
